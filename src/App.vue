6addb929238631abd317a318686d40c9bf0ed400
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-09-05 11:20:53 +0200

    feat(httpClient): Design#10 add configuration Axios

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { DaneoApi } from "@/api/daneoApi";
+
+interface Deck {
+  id: number;
+  name: string;
+  description: string;
+  cardCount: number;
+}
+
+const decks = ref<Deck[]>([]);
+
+onMounted(async () => {
+  const response = await DaneoApi.get<Deck[]>("/decks");
+  decks.value = response.data;
+});
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -7,6 +24,10 @@
     documentation
   </p>
   <p class="font-hangul text-ink-soft">사과, 학교</p>
+
+  <p v-for="value of decks.values()" :key="value.id">
+    {{ value.id}} - {{ value.name }} - {{ value.description }}
+  </p>
 </template>
 
 <style scoped></style>
--- src/api/daneoApi.ts
+++ src/api/daneoApi.ts
@@ -1,7 +1,9 @@
 import axios, {type AxiosInstance} from "axios";
 
-export const daneoAxios = () => {
-  const api : AxiosInstance = axios.create({
-    baseURL : "http://localhost:8080/api"
-  })
-}
+export const DaneoApi: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers : {
+    "Content-Type": "application/json"
+  }
+})
+
