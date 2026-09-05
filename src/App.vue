e48cee4d50474a4cd3cc709138b300a2d250ca17
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-09-05 04:45:44 +0200

    feat(design-9): add typo

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <p class="font-hangul text-ink-soft">사과, 학교</p>
 </template>
 
 <style scoped></style>
