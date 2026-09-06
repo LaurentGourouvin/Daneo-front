4db222113f0fad20954ccbb1346c5c1208acbbf3
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-09-06 22:43:01 +0200

    feat(component): flashcard component

---
+++ src/component/Flashcard.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type {FlashcardSummary} from "@/types/flashcard/FlashcardSummary.ts";
+
+const props = defineProps<FlashcardSummary>()
+</script>
+
+<template>
+  <div
+    class="deck-card-container flex border border-gray-200 bg-surface p-4 m-2 rounded-2xl hover:cursor-pointer">
+    <div class="flex gap-8 w-full">
+      <aside class="flex items-center">
+        <div class="w-16">
+          <img
+            v-if="props.imagePath"
+            :src="props.imagePath"
+            :alt="'image représentant ' + props.frenchTerm"
+          />
+          <div
+            v-else
+            class="w-16 h-16 rounded-2xl bg-[repeating-linear-gradient(135deg,#f5f3ef_0px,#f5f3ef_12px,#ebe7df_12px,#ebe7df_24px)]"
+          >
+          </div>
+        </div>
+      </aside>
+      <section class="w-full">
+        <p class="font-hangul text-2xl">{{ props.koreanTerm }}</p>
+        <p class="font-mono text-primary text-lg">{{ props.romanization }}</p>
+        <p class="font-sans text-sm text-ink-soft">{{ props.frenchTerm }}</p>
+      </section>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/Components.vue
+++ src/views/Components.vue
@@ -4,18 +4,25 @@ import DaneoIntputTextField from "@/component/DaneoIntputTextField.vue";
 import {ref} from "vue";
 import DaneoChips from "@/component/DaneoChips.vue";
 import DeckCard from "@/component/DeckCard.vue";
+import Flashcard from "@/component/Flashcard.vue";
 
 const frenchWord = ref<string>("");
 </script>
 
 <template>
   <DaneoButton label="Générer une carte"></DaneoButton>
-  <DaneoIntputTextField v-model="frenchWord" label="Quel mot veux-tu apprendre ?" id="french-word" />
+  <DaneoIntputTextField v-model="frenchWord" label="Quel mot veux-tu apprendre ?" id="french-word"/>
   <DaneoChips text="Maitrisé" variants="mastered"/>
   <DaneoChips text="A revoir" variants="reviewed"/>
   <DaneoChips text="Difficile" variants="difficult"/>
   <deck-card deck-title="Nourriture"></deck-card>
   <deck-card deck-title="Météo" :cardCount="9"></deck-card>
+  <flashcard :id="1" korean-term="사과" romanization="sagwa" french-term="pomme"
+             image-path="http://localhost:8080/images/optimized-pomme.webp"></flashcard>
+  <flashcard :id="2" korean-term="아보카도" romanization="abokado" french-term="avocat"
+             image-path="http://localhost:8080/images/b3d34c8d-26cc-4055-98f6-8b748d1b03a2.webp"></flashcard>
+  <flashcard :id="3" korean-term="먹다" romanization="meokda" french-term="manger"
+             image-path=""></flashcard>
 </template>
 <style scoped>
 
