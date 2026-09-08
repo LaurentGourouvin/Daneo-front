1b018a3c979f702d1379e7e4ddce28f6c1844465
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-09-08 18:55:28 +0200

    feat(deck-17): add rename, delete, create

--- src/stores/DecksStore.ts
+++ src/stores/DecksStore.ts
@@ -1,8 +1,16 @@
 import {defineStore} from "pinia";
 import type {DeckListItemResponse} from "@/types/deck/DeckListItemResponse.ts";
 import {ref} from "vue";
-import {getDeckById, getDeckDetail, getDecks} from "@/api/DeckService.ts";
+import {
+  createDeck,
+  deleteDeckById,
+  getDeckById,
+  getDeckDetail,
+  getDecks, renameDeckById
+} from "@/api/DeckService.ts";
 import type {DeckResponse} from "@/types/deck/DeckResponse.ts";
+import type {DeckCreateRequest} from "@/types/deck/DeckCreateRequest.ts";
+import type {DeckUpdateRequest} from "@/types/deck/DeckUpdateRequest.ts";
 
 export const useDecksStore = defineStore('decks', () => {
   const decks = ref<DeckListItemResponse[]>([]);
@@ -48,6 +56,57 @@ export const useDecksStore = defineStore('decks', () => {
     }
   }
 
+  async function create(data: DeckCreateRequest) {
+    loading.value = true;
+    error.value = null;
+
+    try {
+      const resp: DeckResponse = await createDeck(data);
+      decks.value = [...decks.value, {...resp, cardCount: 0}];
+    } catch (e) {
+      error.value = "Impossible de crée un nouveau deck.";
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  async function deleteDeck(id: number) {
+    loading.value = true;
+    error.value = null;
+
+    try {
+      await deleteDeckById(id);
+      decks.value = [...decks.value.filter(deck => deck.id != id)]
+    } catch (e) {
+      error.value = "Impossible de supprimer le deck.";
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  async function renameDeck(id: number, data: DeckUpdateRequest) {
+    loading.value = true;
+    error.value = null;
+
+    try {
+      const resp = await renameDeckById(id, data);
+      const oldDeckInformations = getDeckById(id);
+
+      if(oldDeckInformations) {
+        oldDeckInformations.name = data.name;
+        decks.value = [...decks.value.filter(deck => deck.id != id), {...oldDeckInformations}]
+      }
+
+    } catch (e) {
+      error.value = "Impossible de renommer le deck.";
+    } finally {
+      loading.value = false;
+    }
+  }
+
+  function getDeckById(id: number) : DeckListItemResponse | undefined {
+    return decks.value.find(deck => deck.id == id);
+  }
   function totalDecks() {
     return decks.value.length;
   }
@@ -61,5 +120,17 @@ export const useDecksStore = defineStore('decks', () => {
     return totalCards;
   }
 
-  return {decks, fetchDeckList, fetchDeckById, fetchDeckDetailById, loading, error, totalDecks, totalCards}
+  return {
+    decks,
+    fetchDeckList,
+    deleteDeck,
+    renameDeck,
+    fetchDeckById,
+    create,
+    fetchDeckDetailById,
+    loading,
+    error,
+    totalDecks,
+    totalCards
+  }
 })
--- src/views/Deck.vue
+++ src/views/Deck.vue
@@ -4,13 +4,22 @@ import {onMounted, ref} from "vue";
 import {useDecksStore} from "@/stores/DecksStore.ts";
 import type {DeckDetailResponse} from "@/types/deck/DeckDetailResponse.ts";
 import Flashcard from "@/component/Flashcard.vue";
-import {CircleArrowLeft, CircleEllipsis} from 'lucide-vue-next';
+import {CircleArrowLeft, Trash, Settings2} from 'lucide-vue-next';
+import DaneoButton from "@/component/DaneoButton.vue";
+import DaneoInputTextField from "@/component/DaneoInputTextField.vue";
 
+enum ModalType {
+  DELETE = "DELETE",
+  UPDATE = "UPDATE",
+}
 
 const route = useRoute();
 const router = useRouter();
 const deckStore = useDecksStore();
 const deck = ref<DeckDetailResponse | null>(null);
+const showModalDelete = ref<boolean>(false);
+const showModalUpdate = ref<boolean>(false);
+const updatedName = ref<string>("");
 
 onMounted(async () => {
   const id = route.params.id as string;
@@ -19,16 +28,48 @@ onMounted(async () => {
   }
 });
 
+const toggleModal = (toggle: ModalType) => {
+  if (toggle == ModalType.DELETE) {
+    showModalDelete.value = !showModalDelete.value;
+  }
+
+  if (toggle == ModalType.UPDATE) {
+    console.log("update")
+    showModalUpdate.value = !showModalUpdate.value;
+  }
+}
+
 const backToDecksList = () => {
   router.push("/decks")
+}
+
+const deleteDeck = async (id: number) => {
+  await deckStore.deleteDeck(id);
+  toggleModal(ModalType.DELETE);
+  router.push("/decks");
+}
+
+const updateDeck = async (id: number, name: string) => {
+  if (!name || !id) {
+    return;
+  }
+
+  await deckStore.renameDeck(id, {name: name})
+  deck.value = await deckStore.fetchDeckDetailById(id);
+  toggleModal(ModalType.UPDATE)
 }
 </script>
 
 <template>
   <div v-if="deck">
-    <nav class="flex justify-between py-2 px-3 text-primary bg-gray-200 rounded-b-2xl shadow-xl border-b border-b-gray-300">
+    <nav
+      class="flex justify-between py-2 px-3 text-primary bg-gray-200 rounded-b-2xl shadow-xl border-b border-b-gray-300">
       <CircleArrowLeft @click="backToDecksList"/>
-      <CircleEllipsis />
+      <div class="flex gap-1">
+        <settings2 @click="toggleModal(ModalType.UPDATE)"/>
+        <Trash class="text-danger" @click="toggleModal(ModalType.DELETE)"/>
+      </div>
+
     </nav>
     <header class="flex justify-between items-end py-2 px-3 mt-2">
       <h1 class="font-sans text-xl font-semibold">{{ deck.name }}</h1>
@@ -39,6 +80,33 @@ const backToDecksList = () => {
                :romanization="card.romanization" :key="card.id"/>
   </div>
   <p v-else>Chargement…</p>
+
+  <div v-if="showModalDelete && deck"
+       class="flex flex-col justify-center max-h-full min-h-full max-w-full min-w-full absolute top-0 z-99 bg-surface/80">
+    <section class="m-auto p-4 bg-surface border border-gray-300 shadow-2xl rounded-2xl">
+      <h2>Êtes-vous sûr de vouloir supprimer ce deck ?</h2>
+      <div class="flex justify-center gap-3 mt-4">
+        <DaneoButton label="Oui" variant="danger" @click="deleteDeck(deck.id)"/>
+        <DaneoButton label="Non" variant="secondary" @click="toggleModal(ModalType.DELETE)"/>
+      </div>
+    </section>
+  </div>
+
+  <div v-if="showModalUpdate && deck"
+       class="flex flex-col justify-center max-h-full min-h-full max-w-full min-w-full absolute top-0 z-99 bg-surface/80">
+    <section class="m-auto p-4 bg-surface border border-gray-300 shadow-2xl rounded-2xl">
+      <h2>Modification du deck</h2>
+      <div class="mt-4">
+        <DaneoInputTextField id="name" label="nom du deck" :placeholder="deck.name"
+                             v-model="updatedName"/>
+      </div>
+      <div class="flex justify-center gap-3 mt-4">
+        <DaneoButton label="Enregistrer" variant="primary"
+                     @click="updateDeck(deck.id, updatedName)"/>
+        <DaneoButton label="Annuler" variant="secondary" @click="toggleModal(ModalType.UPDATE)"/>
+      </div>
+    </section>
+  </div>
 </template>
 
 <style scoped>
