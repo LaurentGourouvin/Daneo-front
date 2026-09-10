873f3ed8495dc8795f5b038c172e95728b6904e9
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-09-10 19:16:58 +0200

    feat(axios): update config for working in expose -- --host

--- src/api/DaneoAxios.ts
+++ src/api/DaneoAxios.ts
@@ -1,7 +1,11 @@
 import axios, {type AxiosInstance} from "axios";
 
+const baseURL = window.location.hostname === "localhost"
+  ? import.meta.env.VITE_API_BASE_URL
+  : `http://${window.location.hostname}:8080/api`
+
 export const DaneoAxios: AxiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: baseURL,
   headers : {
     "Content-Type": "application/json"
   }
